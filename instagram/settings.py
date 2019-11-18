c41a97e569cf232f295383fecfc7a01c95e3791a
brayooh <brianndichu.bn@gmail.com>
2019-11-18 10:44:27 +0300

    add database details

--- instagram/settings.py
+++ instagram/settings.py
@@ -75,10 +75,11 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'insta_clone',
+        'USER': 'brayooh',
+        'PASSWORD':'brian',
     }
-}
 
 
 # Password validation

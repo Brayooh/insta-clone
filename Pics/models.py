4b62edf83383566eddf0fdf66f0b956dce5df7ad
brayooh <brianndichu.bn@gmail.com>
2019-11-18 14:40:07 +0300

    modify Post class + add save and delete function

--- Pics/models.py
+++ Pics/models.py
@@ -22,3 +22,30 @@ class Profile(models.Model):
             Profile.objects.create(user=instance)
         instance.profile.save()
 
+
+    def save_profile(self):
+        self.save()
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics/')
+    caption = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+
+
+    def __str__(self):
+        return f'{self.author} Post'
+
+    class Meta:
+        db_table = 'post'
+        ordering = ['-created_date']
+
+    def addlikes(self):
+        self.likes.count()
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
